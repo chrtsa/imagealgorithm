1be7c4c9cd39c38f30bc48629ec903019b5b1c12
Christina Tsagkogianni <christina.tsagkogianni.2@ki.se>
2024-11-22 17:29:32 +0100

    hopefully loading my code

--- R/imagefilter.R
+++ R/imagefilter.R
@@ -1,18 +1,76 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   https://r-pkgs.org
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#'@title Image filter
+#'@param bitmap A 3D array representing an image
+#'@return A 3D array representing the filtered image
 
-hello <- function() {
-  print("Hello, world!")
+
+filter_grey <- function(rgb_vector){
+  R <- as.integer(rgb_vector[1])
+  G <- as.integer(rgb_vector[2])
+  B <- as.integer(rgb_vector[3])
+  grey <- round(R * 0.299 + G * 0.587 + B * 0.114)
+  return(grey)
+}
+
+filter_bw <- function(rgb_vector, cut_off = 127){
+  grey <- as.integer(filter_grey(rgb_vector))
+  if(grey < cut_off){
+    return(c(0, 0, 0))
+  } else {
+    return(c(255, 255, 255))
+  }
+}
+
+filter_red <- function(rgb_vector, cut_off = 127) {
+  grey <- as.integer(filter_grey(rgb_vector))
+  if (grey < cut_off) {
+    return(c(255, 0, 0))
+  } else {
+    return(c(0, 0, 0))
+  }
+}
+
+filter_green <- function(rgb_vector, cut_off = 127) {
+  grey <- as.integer(filter_grey(rgb_vector))
+  if (grey < cut_off) {
+    return(c(0, 128, 0))
+  } else {
+    return(c(0, 0, 0))
+  }
+}
+
+filter_blue <- function(rgb_vector,cut_off = 127) {
+  grey <- as.integer(filter_grey(rgb_vector))
+  if (grey < cut_off) {
+    return(c(0, 0, 255))
+  } else {
+    return(c(0, 0, 0))
+  }
+}
+
+
+filter_bitmap <- function(bitmap, filter, ...){
+  raw_bitmap <- as.integer(bitmap) #I will first convert the bitmap to make it easier to work with.
+  dims <- dim(raw_bitmap)
+  #Making sure the input is appropriate
+  if(length(dims) != 3) {
+    stop("The input bitmap must be a 3D array")
+  }
+
+  if(dims[3] != 3){
+    stop("The input bitmap must have 3 channels")
+  }
+
+  # loop over rows and columns
+  for (i in 1:dims[1]){
+    for (j in 1:dims[2]){
+      bitmap[i, j, ] <- filter(bitmap[i, j, ])
+    }
+  }
+  #normalising values to 0-255
+  bitmap <- pmax(pmin(bitmap, 255), 0)
+  rgb_matrix <- apply(bitmap, c(1, 2), function(pixel) {
+    rgb(pixel[1], pixel[2], pixel[3], maxColorValue = 255)
+  })
+  img <- image_read(as.raster(rgb_matrix))
+  return(img)
 }
