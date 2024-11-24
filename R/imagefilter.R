2002ba0477d669dc1027fe666965f1895e70140f
Christina Tsagkogianni <christina.tsagkogianni.2@ki.se>
2024-11-24 19:04:55 +0100

    Updating several corrections to namespace description and functions

--- R/imagefilter.R
+++ R/imagefilter.R
@@ -1,48 +1,122 @@
 #'@title Image filter
-#'@param bitmap A 3D array representing an image
-#'@return A 3D array representing the filtered image
-
-
-filter_grey <- function(rgb_vector){
+#'Applies a colour filter to an image
+#'Requires the "magick" package to work
+#' Apply Grey Filter to an Image
+#'
+#' This function takes an RGB vector and applies a greyscale transformation
+#'
+#' @param rgb_vector A numeric vector of length 3 representing the RGB values of a pixel.
+#' @return A numeric value representing the grey value of the pixel.
+#' @export
+grey <- function(rgb_vector){
   R <- as.integer(rgb_vector[1])
   G <- as.integer(rgb_vector[2])
   B <- as.integer(rgb_vector[3])
   grey <- round(R * 0.299 + G * 0.587 + B * 0.114)
   return(grey)
 }
-
-filter_bw <- function(rgb_vector, cut_off = 127){
-  grey <- as.integer(filter_grey(rgb_vector))
+#' Apply Grey Filter to an Image
+#'
+#' This function takes an RGB vector and transforms it to black and white
+#'
+#' @param rgb_vector A numeric vector of length 3 representing the RGB values of a pixel.
+#' @param cut_off Numeric value, the threshold to determine if the pixel should be black or white (default is 127).
+#' @return A numeric value .
+#' @export
+bw <- function(rgb_vector, cut_off = 127){
+  grey <- as.integer(grey(rgb_vector))
   if(grey < cut_off){
     return(c(0, 0, 0))
   } else {
     return(c(255, 255, 255))
   }
 }
-
-filter_red <- function(rgb_vector, cut_off = 127) {
-  grey <- as.integer(filter_grey(rgb_vector))
-  if (grey < cut_off) {
-    return(c(255, 0, 0))
-  } else {
-    return(c(0, 0, 0))
-  }
-}
-
-filter_green <- function(rgb_vector, cut_off = 127) {
-  grey <- as.integer(filter_grey(rgb_vector))
+#' Apply Red Filter to an Image
+#'
+#' This function applies a red filter to an RGB pixel based on a grey cutoff value.
+#'
+#' @param rgb_vector A numeric vector of length 3 representing the RGB values of a pixel.
+#' @param cut_off Numeric value, the threshold to determine if the pixel should be red (default is 127).
+#' @return A numeric vector of length 3, either c(255, 0, 0) for red or c(0, 0, 0) for black.
+#' @export
+red <- function(rgb_vector, cut_off = 127)
+  #' Apply Green Filter to an Image
+  #'
+  #' This function applies a green filter to an RGB pixel based on a grey cutoff value.
+  #'
+  #' @param rgb_vector A numeric vector of length 3 representing the RGB values of a pixel.
+  #' @param cut_off Numeric value, the threshold to determine if the pixel should be green (default is 127).
+  #' @return A numeric vector of length 3, either c(0, 128, 0) for green or c(0, 0, 0) for black.
+#' @export
+green <- function(rgb_vector, cut_off = 127) {
+  grey <- as.integer(grey(rgb_vector))
   if (grey < cut_off) {
     return(c(0, 128, 0))
   } else {
     return(c(0, 0, 0))
   }
 }
-
-filter_blue <- function(rgb_vector,cut_off = 127) {
-  grey <- as.integer(filter_grey(rgb_vector))
+#' Apply a Blue Filter to an Image
+#'
+#' This function applies a blue filter to an RGB pixel based on a grey cutoff value.
+#'
+#' @param rgb_vector A numeric vector of length 3 representing the RGB values of a pixel.
+#' @param cut_off Numeric value, the threshold to determine if the pixel should be blue (default is 127).
+#' @return A numeric vector of length 3, either c(0, 0, 255) for blue or c(0, 0, 0) for black.
+#' @export
+#' @export
+blue <- function(rgb_vector,cut_off = 127) {
+  grey <- as.integer(grey(rgb_vector))
   if (grey < cut_off) {
     return(c(0, 0, 255))
   } else {
     return(c(0, 0, 0))
   }
+}
+
+
+#' Apply a Filter to an Image
+#'
+#' This function takes an image and applies a filter to each pixel.
+#' The filter is applied by passing each pixel through the provided function.
+#'
+#' @param image A file path to an image that will be processed.
+#' @param filter A function that processes each RGB pixel of the image.
+#' @return An image object with the filter applied.
+#' @details The function uses the magick::image_read() function to read and process the image
+#'Ensure the package is installed and loaded
+#' @export
+image_filter <- function(image, filter) {
+  # Read the image
+  obj <- image_read(image)
+
+  # Convert the image to a bitmap (3D array)
+  bitmap <- as.integer(obj[[1]])  # Convert to 3D array of RGB values
+  dims <- dim(bitmap)  # Get the dimensions (rows, columns, channels)
+
+  # Loop through each pixel in the bitmap
+  for (i in 1:dims[1]) {  # Loop through the rows
+    for (j in 1:dims[2]) {  # Loop through the columns
+      # Extract RGB values of pixels
+      pixel <- bitmap[i, j, ]
+
+      # Apply the filter to the pixel
+      filtered_pixel <- filter(pixel)
+
+      # Assign the filtered pixel back to the bitmap
+      bitmap[i, j, ] <- filtered_pixel
+    }
+  }
+
+  # Normalise the values back to [0, 255]
+  bitmap <- pmax(pmin(bitmap, 255), 0)
+
+  # Convert the bitmap to RGB values and create an RGB matrix
+  rgb_matrix <- apply(bitmap, c(1, 2), function(pixel) {
+    rgb(pixel[1], pixel[2], pixel[3], maxColorValue = 255)
+  })
+
+  img <- image_read(as.raster(rgb_matrix))
+
+  return(img)
 }
